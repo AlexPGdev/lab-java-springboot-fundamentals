b50fa17f271759059954da075c36928849b6ad61
AlexPGdev <alexandru82250@gmail.com>
2025-04-05 15:06:02 +0200

    finish time task

--- labweek4/src/main/java/com/example/ironhack/labweek4/controller/TimeController.java
+++ labweek4/src/main/java/com/example/ironhack/labweek4/controller/TimeController.java
@@ -1,4 +1,35 @@
 package com.example.ironhack.labweek4.controller;
 
+import com.example.ironhack.labweek4.service.TimeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TimeController {
+
+    private final TimeService timeService;
+
+    public TimeController(TimeService timeService) {
+        this.timeService = timeService;
+    }
+
+    @GetMapping("/time")
+    public String getCurrentTime() {
+        return "Current time is " + timeService.getCurrentTime();
+    }
+
+    @GetMapping("/date")
+    public String getCurrentDate() {
+        return "Current date is " + timeService.getCurrentDate();
+    }
+
+    @GetMapping("/day")
+    public String getDayOfWeek() {
+        return "Today is " + timeService.getDayOfWeek();
+    }
+
+    @GetMapping("/all")
+    public String getAll() {
+        return timeService.getAll();
+    }
 }
--- labweek4/src/main/java/com/example/ironhack/labweek4/service/TimeService.java
+++ labweek4/src/main/java/com/example/ironhack/labweek4/service/TimeService.java
@@ -1,4 +1,34 @@
 package com.example.ironhack.labweek4.service;
 
+import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+
+
+@Service
 public class TimeService {
+
+    public LocalTime getCurrentTime() {
+        // Show only hh:mm:ss
+        return LocalTime.now().truncatedTo(ChronoUnit.SECONDS);
+    }
+
+    public LocalDate getCurrentDate() {
+        return LocalDate.now();
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return LocalDate.now().getDayOfWeek();
+    }
+
+    public String getAll() {
+        return "Current Date: " + LocalDate.now() + "\n" +
+                "Current Time: " + LocalTime.now() + "\n" +
+                "Current Day: " + LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+    }
+
 }
